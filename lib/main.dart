64e16ed53b8d6a0d71ef1b2c1aecf4ed6dbbdfab
David-B-Comer <davec2233@gmail.com>
2020-06-30 15:43:54 -0400

    added google sign in import

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
-
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() => runApp(MyApp());
 
