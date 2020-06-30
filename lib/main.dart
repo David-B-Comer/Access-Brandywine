ad6061f3d346259259d3e2f99fd95872c7519b09
David-B-Comer <davec2233@gmail.com>
2020-06-30 14:47:50 -0400

    added facebook login import

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 
 
 void main() => runApp(MyApp());
